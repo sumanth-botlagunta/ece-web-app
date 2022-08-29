1139011da6a1edee9355c65646dafa5ebf5b432c
sumanth-botlagunta <sumanthbotlagunta977@gmail.com>
2022-08-29 12:24:12 +0530

    navbar

---
+++ src/components/navbar/Header.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import "./navbar.css";
+import { CSSTransition } from "react-transition-group";
+
+export default function Header() {
+  const [isNavVisible, setNavVisibility] = useState(false);
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 700px)");
+    mediaQuery.addListener(handleMediaQueryChange);
+    handleMediaQueryChange(mediaQuery);
+
+    return () => {
+      mediaQuery.removeListener(handleMediaQueryChange);
+    };
+  }, []);
+
+  const handleMediaQueryChange = (mediaQuery) => {
+    if (mediaQuery.matches) {
+      setIsSmallScreen(true);
+    } else {
+      setIsSmallScreen(false);
+    }
+  };
+
+  const toggleNav = () => {
+    setNavVisibility(!isNavVisible);
+  };
+
+  return (
+    <header className="Header">
+      <img src={require("../../assets/logo.png")} className="Logo" alt="logo" />
+      <CSSTransition
+        in={!isSmallScreen || isNavVisible}
+        timeout={350}
+        classNames="NavAnimation"
+        unmountOnExit
+      >
+        <nav className="Nav">
+          <a href="/">Home</a>
+          <a href="/">Articles</a>
+          <a href="/">About</a>
+          <a href="/">Logout</a>
+        </nav>
+      </CSSTransition>
+      <button onClick={toggleNav} className="Burger">
+        <span role="img" aria-label="">
+          {" "}
+          😵{" "}
+        </span>
+      </button>
+    </header>
+  );
+}
