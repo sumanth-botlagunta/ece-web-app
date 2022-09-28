3d453da3631de0cb660b8d79e30cc7ae8725d93f
Madhu <madhukarreddy.suguru@gmail.com>
2022-09-28 22:14:42 +0530

    carousal removed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import Navbar from './components/navbar/Header';
-import Carousal from './components/carousal/Carousal';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <Carousal />
     </div>
   );
 }
