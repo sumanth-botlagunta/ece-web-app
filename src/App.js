d910247ad311b709c64f20ec047b1cc4769c7b6d
sumanth-botlagunta <sumanthbotlagunta977@gmail.com>
2022-09-30 01:23:18 +0530

    Hearld section added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Welcome from './components/welcome/Welcome';
 import Events from './components/events/Events';
+import Hearld from "./components/hearld/Hearld";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Welcome />
       <Events />
+      <Hearld />
     </div>
   );
 }
